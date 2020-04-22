41198fcaee58dd562053089388625c2aa8bb0e83
Sabene Pleasant <spleasan@cs.wellesley.edu>
2020-04-22 19:56:18 -0400

    V1. of the application. Function stubs.

---
+++ app.py
@@ -0,0 +1,61 @@
+from flask import (Flask, render_template, make_response, url_for, request,
+                   redirect, flash, session, send_from_directory, jsonify)
+from werkzeug.utils import secure_filename
+app = Flask(__name__)
+
+# one or the other of these. Defaults to MySQL (PyMySQL)
+# change comment characters to switch to SQLite
+
+import cs304dbi as dbi
+# import cs304dbi_sqlite3 as dbi
+
+import random
+
+app.secret_key = 'your secret here'
+# replace that with a random key
+app.secret_key = ''.join([ random.choice(('ABCDEFGHIJKLMNOPQRSTUVXYZ' +
+                                          'abcdefghijklmnopqrstuvxyz' +
+                                          '0123456789'))
+                           for i in range(20) ])
+
+# This gets us better error messages for certain common request errors
+app.config['TRAP_BAD_REQUEST_ERRORS'] = True
+
+
+@app.route('/')
+def index():
+    return render_template('base.html',title='Home')
+#Need dbs 
+@app.route('/add/', methods=["GET", "POST"])
+def add():
+    return render_template('base.html',title='Home')
+
+@app.route('/search/', methods=["GET", "POST"])
+def search():
+    return render_template('base.html',title='Home')
+
+@app.route('/favorites/', methods=["GET", "POST"])
+def favorites():
+    return render_template('base.html',title='Home')
+
+
+if __name__ == '__main__':
+    import sys, os
+    if len(sys.argv) > 1:
+        # arg, if any, is the desired port number
+        port = int(sys.argv[1])
+        assert(port>1024)
+    else:
+        port = os.getuid()
+    # the following database code works for both PyMySQL and SQLite3
+    dbi.cache_cnf()
+    dbi.use('wmdb')
+    conn = dbi.connect()
+    curs = dbi.dict_cursor(conn)
+    # the following query works for both MySQL and SQLite
+    curs.execute('select current_timestamp as ct')
+    row = curs.fetchone()
+    ct = row['ct']
+    print('connected to WMDB at {}'.format(ct))
+    app.debug = True
+    app.run('0.0.0.0',port)
